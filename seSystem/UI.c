6f29ecb8f3ad57902d046d6ce5beea0d4063a9cd
murthy's workstation <murthysanchez@163.com>
2019-12-21 16:03:17 +0800

    add keyboard example

--- seSystem/UI.c
+++ seSystem/UI.c
@@ -4,13 +4,14 @@
 #include "UI.h"
 /*UI界面最新进展
  * 窗口搭建完成
- * 下一步：
- *          添加输入框
+ * 退出按钮ok
+ * 
  * */
 
 void delete_event( GtkWidget *widget, GdkEvent  *event, gpointer   data )
 { 
     g_print ("user clicked exit\n");
+    gtk_main_quit();
   
 }
 
@@ -25,6 +26,7 @@ int UI(char name[]){
     gtk_window_set_title(GTK_WINDOW(window),name);   //设置标题
     gtk_window_set_default_size(GTK_WINDOW(window),750,550);    //设置大小
     gtk_window_set_position(GTK_WINDOW(window),GTK_WIN_POS_CENTER); //设置窗口位置
+    gtk_container_set_border_width (GTK_CONTAINER (btn_cls)), 20);
 
     g_signal_connect(G_OBJECT(window),"destroy",G_CALLBACK(gtk_main_quit),NULL);//事件监听，检测用户关闭windows时窗口关闭
 
@@ -33,7 +35,7 @@ int UI(char name[]){
     /*按钮初始化*/
     btn_cls=gtk_button_new_with_label("exit");
     g_signal_connect(G_OBJECT(btn_cls),"clicked",G_CALLBACK(delete_event),window);
-    g_signal_connect_swapped(G_OBJECT(btn_cls),"clicked",G_CALLBACK(gtk_widget_destroy),window);   //点击弹出退出
+    // g_signal_connect_swapped(G_OBJECT(btn_cls),"clicked",G_CALLBACK(gtk_widget_destroy),window);   //点击弹出退出
     gtk_container_add(GTK_CONTAINER(window),btn_cls);   //将btn加入
     gtk_widget_show(btn_cls);   //显示按钮
 
