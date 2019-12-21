1209292776b378094e757c13439f522bc63df393
murthy's workstation <murthysanchez@163.com>
2019-12-21 19:40:42 +0800

    add gtk label

--- seSystem/UI.c
+++ seSystem/UI.c
@@ -5,7 +5,8 @@
 /*UI界面最新进展
  * 窗口搭建完成
  * 退出按钮ok
- * 
+ * 设置窗口为19*27的格子
+ *  添加系统logo 大小：125*89p
  * */
 
 void delete_event( GtkWidget *widget, GdkEvent  *event, gpointer   data )
@@ -15,17 +16,18 @@ void delete_event( GtkWidget *widget, GdkEvent  *event, gpointer   data )
   
 }
 
-int UI(char name[]){
+int UI(char* name){
     GtkWidget* window;//定义窗口
     GtkWidget* button;//定义按钮
     GtkWidget* table;//定义table
+    GtkWidget* image;//image
     gtk_init(NULL,NULL);//初始化
 
     /*窗口初始化*/
     /*********************/
     window=gtk_window_new(GTK_WINDOW_TOPLEVEL); //创建一个置顶窗口
     gtk_window_set_title(GTK_WINDOW(window),name);   //设置标题
-    gtk_window_set_default_size(GTK_WINDOW(window),846,600);    //设置大小4134x5846
+    gtk_window_set_default_size(GTK_WINDOW(window),846,600);    //设置大小
     gtk_window_set_position(GTK_WINDOW(window),GTK_WIN_POS_CENTER); //设置窗口位置
     gtk_container_set_border_width (GTK_CONTAINER (window), 20);    //设置窗口边框宽度
     g_signal_connect(G_OBJECT(window),"destroy",G_CALLBACK(gtk_main_quit),NULL);//事件监听，检测用户关闭windows时窗口关闭
@@ -33,6 +35,13 @@ int UI(char name[]){
 
     table=gtk_table_new(19,27,TRUE);
     gtk_container_add(GTK_CONTAINER(window),table);
+
+    /*logo*/
+    /*********************/
+    image=gtk_image_new_from_file("./image/icon.png");
+    gtk_table_attach_defaults(GTK_TABLE(table),image,9,19,3,7);
+    gtk_widget_show(image);
+    /*********************/
 
     /*退出按钮*/
     /*********************/
@@ -50,6 +59,6 @@ int UI(char name[]){
 }
 
 int main(){
-    char name[50]="Company Attendance System";
+    char* name="Company Attendance System";
     UI(name);
 }
